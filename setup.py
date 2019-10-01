5c98faedb3771fd3d73b64ea47498fa687c7f695
Ben Gotthold <bgotthold@usgs.gov>
2019-10-01 09:57:53 -0600

    premission

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='usnvc',
+      version='0.1',
+      description='usnvc pipline processing',
+      url='https://github.com/bgotthold-usgs/pipeline_USNVC',
+      author='Ben Gotthold, Sky Bristol',
+      author_email='',
+      license='MIT',
+      packages=['usnvc'],
+      zip_safe=False)
